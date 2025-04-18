66e2de78b245d9341aece6d09afb2cea7413617d
paulo-rago <149490954+paulo-rago@users.noreply.github.com>
2025-04-18 20:16:26 -0300

    funcionario service

---
+++ portallicitante/src/main/java/com/vrio/portallicitante/service/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,49 @@
+package com.vrio.portallicitante.service.impl;
+
+import com.vrio.portallicitante.model.Funcionario;
+import com.vrio.portallicitante.repository.FuncionarioRepository;
+import com.vrio.portallicitante.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void cadastrar(Funcionario funcionario) {
+        funcionario.setSenha(passwordEncoder.encode(funcionario.getSenha()));
+        funcionarioRepository.salvar(funcionario);
+    }
+
+    @Override
+    @Transactional
+    public void atualizar(Funcionario funcionario) {
+        funcionarioRepository.atualizar(funcionario);
+    }
+
+    @Override
+    @Transactional
+    public void deletar(int id) {
+        funcionarioRepository.deletar(id);
+    }
+
+    @Override
+    public Optional<Funcionario> autenticar(String cpf, String senha) {
+        Optional<Funcionario> funcionarioOpt = funcionarioRepository.buscarPorCpf(cpf);
+        if (funcionarioOpt.isPresent() &&
+                passwordEncoder.matches(senha, funcionarioOpt.get().getSenha())) {
+            return funcionarioOpt;
+        }
+        return Optional.empty();
+    }
+}
